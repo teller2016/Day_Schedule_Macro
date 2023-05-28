7c156b0e406cdc4fbaeadc5b11fb8ae3ff77a328
teller2016 <teller75@naver.com>
2023-05-29 01:07:31 +0900

    feat: - ignore Alert - activate Schedule Function

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 require("dotenv").config();
-const puppeteer = require("puppeteer-core");
+const puppeteer = require("puppeteer");
 
 const bizboxURL = "https://gw.forbiz.co.kr/gw/uat/uia/egovLoginUsr.do";
 
 class PageMacro {
   constructor(page) {
     this.page = page;
+  }
+
+  ignoreAlert() {
+    // dialog 이벤트 핸들러 등록 => alert창 뜰때마다 실행
+    this.page.on("dialog", async (dialog) => {
+      console.log(`Ignored Alert Message: ${dialog.message()}`);
+      await dialog.dismiss(); // alert 창 닫기
+    });
   }
 
   async enterValueToElement(target, inputValue) {
@@ -16,6 +24,7 @@ class PageMacro {
     );
   }
 
+  // 비즈박스 로그인
   async login(id, password) {
     await this.enterValueToElement("#userId", id);
     await this.enterValueToElement("#userPw", password);
@@ -23,18 +32,36 @@ class PageMacro {
     await this.page.click(".login_submit");
   }
 
+  // 일정 페이지로 이동
   async moveToSchedulePage() {
     await this.page.waitForSelector("#topMenu300000000");
     await this.page.click("#topMenu300000000");
 
     await this.page.waitForXPath("//*[text()='[플본] FE파트']"); // '[플본] FE파트'라는 텍스트가 있는 요소가 나타날 때까지 대기합니다.
     const elements = await this.page.$x("//*[text()='[플본] FE파트']"); // '[플본] FE파트'라는 텍스트가 있는 모든 요소를 XPath를 통해 찾습니다.
-    // 첫 번째 요소를 클릭합니다.
+    // [FE]파트 페이지로 이동
     if (elements.length > 0) {
       await elements[0].click();
     }
+  }
 
-    // await this.page.click(".fc-agendaDay-button");
+  // 일정 등록 함수 활성화처리
+  async activateScheduleInsertFunction() {
+    const iframeElement = await this.page.$("iframe");
+    const frame = await iframeElement.contentFrame();
+
+    await frame.waitForSelector("[data-time='00:00:00'] > .fc-widget-content");
+    // 00시 테이블 열
+    const rows = await frame.$$("[data-time='00:00:00'] > .fc-widget-content");
+
+    // console.log(rows[1]);
+
+    setTimeout(async () => {
+      await rows[1].click();
+
+      await frame.waitForSelector("#popupInsertClose");
+      await frame.click("#popupInsertClose");
+    }, 3000);
   }
 }
 
@@ -44,8 +71,8 @@ class PageMacro {
     headless: false,
 
     // 크롬이 설치된 위치를 입력해줍니다. 엣지 등 크로미움 기반의 웹브라우저도 지원됩니다.
-    executablePath:
-      "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
+    // executablePath:
+    //   "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
   });
   const page = await browser.newPage();
 
@@ -60,33 +87,26 @@ class PageMacro {
 
   const pageMacro = new PageMacro(page);
 
+  // 알럿 무시
+  pageMacro.ignoreAlert();
+
   // 로그인
   await pageMacro.login(process.env.BIZBOX_ID, process.env.BIZBOX_PASSWORD);
 
   // 일정 페이지로 이동
   await pageMacro.moveToSchedulePage();
 
-  // .b 요소가 로드될때 까지 대기
-  //   await page.waitForSelector(".b");
-  //   await page.click(".b");
-
-  // dialog 이벤트 핸들러 등록 => alert창 뜰때마다 실행
-  page.on("dialog", async (dialog) => {
-    console.log(`Dialog message: ${dialog.message()}`);
-    await dialog.dismiss(); // alert 창 닫기
-  });
+  // 일정 함수 실행시킴 (일정 등록 함수 활성화 하기 위함)
+  await pageMacro.activateScheduleInsertFunction();
 
   //   window 함수 접근후 실행
   await page.evaluate(() => {
-    window.alert("asdfl");
+    // window.alert("I am Alert");
+    // console.log(window.wrapWindowByMaskInsert);
   });
 
   // 키보드 입력
   //   await page.keyboard.press("Enter");
-
-  // 스크린샷
-  //fullPage:false로 하면 현재 브라우저에서 보이는 영역만 스크린캡쳐를 뜨게 됩니다.
-  //await page.screenshot({ path: "example.png", fullPage: true });
 
   // 브라우저 닫기
   //await browser.close();
